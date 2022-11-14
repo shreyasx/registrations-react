aa7b985bb0e78a7450a26dffa8462ef8c3209b99
Shreyas Jamkhandi <shreyxs@gmail.com>
2022-11-14 08:50:10 +0530

    first commit

---
+++ src/components/private/Private.js
@@ -0,0 +1,124 @@
+import React from "react";
+import axios from "axios";
+import { Button } from "@material-ui/core";
+import LoadingButton from "@mui/lab/LoadingButton";
+
+class Private extends React.Component {
+	constructor() {
+		super();
+		this.state = { admin: false, approved: false, users: [], loading: true };
+	}
+
+	componentDidMount() {
+		if (!localStorage.getItem("authToken")) {
+			this.props.history.push("/login");
+		}
+
+		const fetchPrivateData = async () => {
+			try {
+				const { data } = await axios.get("/api/private", {
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+					},
+				});
+				this.setState(data);
+				if (data.admin) this.fetchUsers();
+			} catch (error) {
+				localStorage.removeItem("authToken");
+				this.props.history.push("/login");
+			}
+		};
+
+		fetchPrivateData();
+	}
+
+	render() {
+		return (
+			<>
+				{this.state.admin ? (
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "centre",
+							margin: 50,
+						}}
+					>
+						<table className="tg">
+							<thead>
+								<tr>
+									<th className="tg-0lax">Name</th>
+									<th className="tg-0lax">USN</th>
+									<th className="tg-0lax">Email</th>
+									<th className="tg-0lax">UPI Reference number</th>
+									<th className="tg-0lax">Approve Payment</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.users.map((user, index) => (
+									<tr key={index}>
+										<td className="tg-0lax">{user.name}</td>
+										<td className="tg-0lax">{user.usn}</td>
+										<td className="tg-0lax">{user.email}</td>
+										<td className="tg-0lax">{user.upi_ref_no}</td>
+										<td className="tg-0lax">
+											<LoadingButton
+												size="small"
+												variant="outlined"
+												loading={this.state.loading}
+												color="primary"
+												onClick={() => this.approve(user.usn)}
+											>
+												Approve
+											</LoadingButton>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				) : (
+					<>{this.state.approved ? "approved" : "not app"}</>
+				)}
+				<div style={{ display: "flex", justifyContent: "center" }}>
+					<Button variant="contained" color="primary" onClick={this.logout}>
+						Logout
+					</Button>
+				</div>
+			</>
+		);
+	}
+
+	logout = () => {
+		localStorage.removeItem("authToken");
+		this.props.history.push("/login");
+	};
+
+	fetchUsers = async () => {
+		const { data } = await axios.get("/api/private/list", {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+			},
+		});
+		this.setState({ users: data, loading: false });
+	};
+
+	approve = async usn => {
+		this.setState({ loading: true });
+		const { data } = await axios.put(
+			"/api/private/approve",
+			{ usn },
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+				},
+			}
+		);
+		if (data.success) this.fetchUsers();
+	};
+}
+
+export default Private;
---
+++ src/components/register/Register.js
@@ -0,0 +1,249 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import styles from "./styles";
+import { withStyles } from "@material-ui/core/styles";
+import axios from "axios";
+
+class Register extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			name: null,
+			usn: null,
+			phone: null,
+			email: null,
+			password: null,
+			passwordConfirmation: null,
+			upi_ref_no: null,
+			signupError: "",
+		};
+	}
+
+	//once user is logged, it doesnt make sense to come on login/register screen
+	componentDidMount() {
+		if (localStorage.getItem("authToken")) {
+			this.props.history.push("/");
+		}
+	}
+
+	render() {
+		const { classes } = this.props;
+
+		return (
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Typography component="h1" variant="h3">
+						Sign up
+					</Typography>
+					<form onSubmit={e => this.submitSignup(e)} className={classes.form}>
+						<Grid container spacing={2}>
+							<Grid item xs={12}>
+								<TextField
+									style={{ color: "#ffffff" }}
+									autoFocus
+									variant="outlined"
+									required={true}
+									fullWidth
+									id="name"
+									label="Enter Name"
+									name="username"
+									autoComplete="off"
+									onChange={e => this.userTyping("name", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									style={{ color: "#ffffff" }}
+									autoFocus
+									variant="outlined"
+									required={true}
+									fullWidth
+									id="usn"
+									label="Enter USN"
+									name="USN"
+									autoComplete="off"
+									onChange={e => this.userTyping("usn", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									style={{ color: "#ffffff" }}
+									variant="outlined"
+									required={true}
+									fullWidth
+									id="phone"
+									label="Enter Phone number"
+									name="phone"
+									autoComplete="off"
+									onChange={e => this.userTyping("phone", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									style={{ color: "#ffffff" }}
+									variant="outlined"
+									required={true}
+									fullWidth
+									id="email"
+									label="Enter Email Address"
+									name="email"
+									autoComplete="off"
+									onChange={e => this.userTyping("email", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required={true}
+									fullWidth
+									name="password"
+									label="Enter Password"
+									type="password"
+									id="password"
+									autoComplete="off"
+									onChange={e => this.userTyping("password", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									variant="outlined"
+									required={true}
+									fullWidth
+									name="passwordConfirmation"
+									label="Confirm Password"
+									type="password"
+									id="password-confirmation"
+									onChange={e => this.userTyping("passwordConfirmation", e)}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									style={{ color: "#ffffff" }}
+									autoFocus
+									variant="outlined"
+									required={true}
+									fullWidth
+									id="upi_ref_no"
+									label="Enter UPI Reference number"
+									name="upi_ref_no"
+									autoComplete="off"
+									onChange={e => this.userTyping("upi_ref_no", e)}
+								/>
+							</Grid>
+							{this.state.signupError ? (
+								<Grid container justify="center">
+									<Grid item>
+										<Typography className={classes.errorText} variant="body2">
+											{this.state.signupError}
+										</Typography>
+									</Grid>
+								</Grid>
+							) : null}
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							className={classes.submit}
+						>
+							Sign Up
+						</Button>
+						<Grid container justify="center">
+							<Grid item>
+								<Link
+									className={classes.link}
+									href="#"
+									variant="body2"
+									to="/login"
+								>
+									Already have an account?{" "}
+									<span className={classes.signIn}>Sign in</span>
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
+		);
+	}
+
+	formIsValid = () =>
+		this.state.password === this.state.passwordConfirmation &&
+		this.state.password.length > 4;
+
+	userTyping = (type, e) => {
+		switch (type) {
+			case "name":
+				this.setState({ name: e.target.value });
+				break;
+			case "usn":
+				this.setState({ usn: e.target.value });
+				break;
+			case "email":
+				this.setState({ email: e.target.value });
+				break;
+			case "password":
+				this.setState({ password: e.target.value });
+				break;
+			case "phone":
+				this.setState({ phone: e.target.value });
+				break;
+			case "passwordConfirmation":
+				this.setState({ passwordConfirmation: e.target.value });
+				break;
+			case "upi_ref_no":
+				this.setState({ upi_ref_no: e.target.value });
+				break;
+			default:
+				break;
+		}
+	};
+
+	submitSignup = async e => {
+		e.preventDefault();
+
+		const config = {
+			header: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		if (!this.formIsValid()) {
+			this.setState({
+				signupError:
+					"Passwords must match and must be greater than 4 characters.",
+			});
+
+			setTimeout(() => {
+				this.setState({ signupError: " " });
+			}, 5000);
+			return;
+		}
+
+		const { name, usn, email, password, phone, upi_ref_no } = this.state;
+		try {
+			const { data } = await axios.post(
+				"http://localhost:5001/api/auth/register",
+				{ name, usn, email, phone, password, upi_ref_no },
+				config
+			);
+			localStorage.setItem("authToken", data.token);
+			this.props.history.push("/");
+		} catch (error) {
+			console.log(error);
+			this.setState({ error });
+			setTimeout(() => {
+				this.setState({ signupError: "" });
+			}, 5000);
+		}
+	};
+}
+
+export default withStyles(styles)(Register);
